ccfef390be0fc4f5471511939a610436835a54d8
Richard Hounslow <rhounslow@gmail.com>
2023-04-26 15:37:11 +1000

    make test explicit boolean

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,7 +17,7 @@ const Home: NextPage = () => {
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         {!user.isSignedIn && <SignIn path="/" routing="path" signUpUrl="/" />}
-        {user.isSignedIn && <SignOutButton />}
+        {!!user.isSignedIn && <SignOutButton />}
       </main>
     </>
   );
